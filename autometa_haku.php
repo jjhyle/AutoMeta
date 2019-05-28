0274fb103adb3f5fcee3a7dfa543b71e278b5ca8
jjhyle <50319876+jjhyle@users.noreply.github.com>
2019-05-28 14:17:21 +0300

    Add files via upload

--- autometa_haku.php
+++ autometa_haku.php
@@ -1,3 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table, td, th {
+  border: 1px solid orangered;
+}
+
+table {
+  border-collapse: collapse;
+  width: 50%;
+}
+
+th {
+  text-align: left;
+}
+</style>
+</head>
+<body>
+
+
 <?php
 
 $dbhost = "localhost";
@@ -14,12 +35,9 @@ $mysqli = new mysqli("$dbhost", "$dbuser", "$dbpass", "$dbname");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-print "test	";
+//print "test	";
 // Retrieve data from Query String
 $sana = $_GET['sana'];
-echo "sananalku";
-echo $sana;
-echo "sananloppu";
 
 // Escape User Input to help prevent SQL Injection
 //$age = mysql_real_escape_string($age);
@@ -27,7 +45,7 @@ echo "sananloppu";
 //$wpm = mysql_real_escape_string($wpm);
 	
 //build query
-$query = "SELECT * FROM haku WHERE keyword like '%$sana%'";
+$query = "SELECT * FROM haku WHERE keyword LIKE '%$sana%' ORDER BY user_id, keyword, picture_id";
 
 	
 //Execute query
@@ -42,19 +60,21 @@ $display_string .= "<tr>";
 $display_string .= "<th>Keyword</th>";
 $display_string .= "<th>Picture_id</th>";
 $display_string .= "<th>User_id</th>";
-$display_string .= "</tr>";
+$display_string .= "</tr>"; 
 
-// Insert a new row in the table for each person returned
+// Insert a new row in the table for each keyword returned
 //while($row = mysqli_fetch_array($qry_result)) {
  while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
    $display_string .= "<tr>";
-   $display_string .= "<td>$row[keyword]</td>";
-   $display_string .= "<td>$row[picture_id]</td>";
-   $display_string .= "<td>$row[user_id]</td>";
-   $display_string .= "</tr>";
+   $display_string .= "<td><font color=#f35656 size=6>$row[keyword]</td>";
+   $link_id = $row[picture_id];
+   $display_string .= "<td><a href=$link_id>$row[picture_id]<img src=$link_id style=width:50px;height:50px;></a></td>";
+   $display_string .= "<td>$row[user_id]</td>";         
+//   $display_string .= "<td>$row<a href='https://www.google.com/'>demo text</a></td>";
+   $display_string .= "</tr>";  
 }
 
-echo "Query: " . $query . "<br />";
+//echo "Query: " . $query . "<br />";
 $display_string .= "</table>";
 
 echo $display_string;
